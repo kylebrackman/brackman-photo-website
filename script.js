167f958b895ae87dddfdb254a40d4bd81053f4b7
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-08-28 16:48:39 -0600

    Changed header

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const h2 = document.createElement("h2");
 
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Kyle Brackman Photography";
 
 document.querySelector("body").appendChild(h2);
 
